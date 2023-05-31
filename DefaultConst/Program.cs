42ad9f186177728834a07da45d48ef3f2e90a965
beverley-dick <beverleydick11@gmail.com>
2023-05-31 09:38:16 +0300

    Add project files.

---
+++ DefaultConst/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DefaultConst
+{
+    class Program
+    {
+        int a;
+
+        static void Main(string[] args)
+        {
+            //Call default constructor
+            Program p1 = new Program();
+
+            Console.WriteLine("Default value of a: " + p1.a);
+            Console.ReadLine();
+
+        }
+    }
+}
